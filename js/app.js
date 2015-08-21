393143d47e83e119e2abba3ac2a6164e2236c3aa
bcasadei <bill@billcasadei.com>
2015-08-20 20:39:07 -0600

    Refactored with fizzBuzz  function.

--- js/app.js
+++ js/app.js
@@ -2,31 +2,51 @@
  * Created by bc on 8/15/15.
  */
 $(document).ready(function() {
-    var counter = 1;
-    // Loop that counts from 1 to 100
-    while (counter <= 100) {
-        console.log(counter);
-        // Print number if not divisable by 3 or 5
-        if ((counter % 3 !== 0) && (counter % 5 !== 0)) {
-            console.log("Number is not divisable by 3 or 5");
-            $("body").append("<p>" + counter + "</p>");
+   //DEBUGGER
+   /*  var DEBUG_MODE = false;
+    var debug = function(msg) {
+        if (DEBUG_MODE == true) {
+            console.log("DEBUG:", msg);
         }
-        // Print Fizz Buzz if divisable by 3 and 5
-        else if ((counter % 3 === 0) && (counter % 5 === 0)) {
-            console.log("Number is divisable by 3 and 5");
-            $("body").append("<p>Fizz Buzz</p>");
-        }
-        // Print Fizz if divisable by 3
-        else if (counter % 3 === 0) {
-            console.log("Number is divisable by 3");
-            $("body").append("<p>Fizz</p>");
-        }
-        // Print Buzz if divisable by 5
-        else if (counter % 5 === 0) {
-            console.log("Number is divisable by 5");
-            $("body").append("<p>Buzz</p>");
-        }
-        // ??? Why can't the counter be at the top and start at 0?
-        counter++;
+    } */
+
+    //Prompt for number
+    var num = prompt("Enter a number");
+    var n = +num;
+    // Validate that n is a number
+    if (n === parseInt(n, 10)) {
+        fizzBuzz(n);
     }
+    else {
+        prompt("Please only enter numbers");
+    }
+
+    // fizzBuzz Function
+    function fizzBuzz(n) {
+        var counter = 1;
+        while (counter <= n) {
+            // Print number if not divisable by 3 or 5
+            if ((counter % 3 !== 0) && (counter % 5 !== 0)) {
+                $("body").append("<p>" + counter + "</p>");
+            }
+            // Print Fizz Buzz if divisable by 3 and 5
+            else if ((counter % 15 === 0)) {
+                $("body").append("<p>Fizz Buzz</p>");
+            }
+            // Print Fizz if divisable by 3
+            else if (counter % 3 === 0) {
+                $("body").append("<p>Fizz</p>");
+            }
+            // Print Buzz if divisable by 5
+            else if (counter % 5 === 0) {
+                $("body").append("<p>Buzz</p>");
+            }
+            // ??? Why can't the counter be at the top and start at 0?
+            counter++;
+        }
+    }
+
+
+
+
 });
